c74d39a7b56e467d80b9079bc6db4f18f6ddd560
Saurav Tomar <stomatrix@gmail.com>
2016-10-11 13:19:35 +0000

    fixed small bugs

--- app/views.py
+++ app/views.py
@@ -13,11 +13,13 @@ SERVER_URL = 'http://chestream.cloudapp.net:8080/akachan'
 
 @app.route('/')
 def home():
-    x1 = request.args['x1'] or 0
-    x2 = request.args['x1'] or 0
-    y1 = request.args['y1'] or 12
-    y2 = request.args['y2'] or 13
-    return make_gif()
+    print 'San Francisco'
+    x1 = request.args.get('x1') or 0 
+    x2 = request.args.get('x1') or 0
+    y1 = request.args.get('y1') or 12
+    y2 = request.args.get('y2') or 13
+    print x1,x2,y1,y2
+    return make_gif(x1=int(x1),y1=int(y1),x2=int(x2),y2=int(y2))
 
 def main():
     if os.path.exists("%s/permutations"%(DIR_PATH)):
@@ -33,11 +35,12 @@ def main():
 
 
 def make_gif(x1=0,y1=50,x2=10,y2=39):
-    assert (x1 >= 0 and y1 >=0 and x2 >= 0 and y2 >=0 and x1 < 100 and x2 < 100 and y1 < 100 and y2 < 100 ),"Coordinates must be between [0,0]&[99,99]"
-
+    print "Calling make gif"
+    #assert (x1 >= 0 and y1 >=0 and x2 >= 0 and y2 >=0 and x1 < 100 and x2 < 100 and y1 < 100 and y2 < 100 ),"Coordinates must be between [0,0]&[99,99]"
+    #return "%s/animations"%(DIR_PATH)
     if not os.path.exists("%s/animations"%(DIR_PATH)):
         os.system('mkdir %s/animations'%(DIR_PATH))
-
+    print 'SF'
     if os.path.exists("%s/permutations/%03dx%03d-%03dx%03d.gif"%(DIR_PATH,x1,y1,x2,y2)):
         return "%s/%03dx%03d-%03dx%03d.gif"%(SERVER_URL,x1,y1,x2,y2)
 
@@ -47,11 +50,13 @@ def make_gif(x1=0,y1=50,x2=10,y2=39):
         image_string = image_string + ' %s/permutations/%03dx%03d.png'%(DIR_PATH,i[0],i[1])
     
     cmd = 'convert -delay 10 %s -loop 0 %s/animations/%03dx%03d-%03dx%03d.gif'%(image_string,DIR_PATH,x1,y1,x2,y2)
+    print(cmd)
     os.system(cmd)
-
+    print("Reaching the end")
     return "%s/%03dx%03d-%03dx%03d.gif"%(SERVER_URL,x1,y1,x2,y2)
     
 
 
 if __name__ == '__main__':
+    #main()
     app.run(debug=True,host='0.0.0.0')
